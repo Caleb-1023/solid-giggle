8b0ef5f92ba75b39052c57115e9b9d5b3f5eddfc
Caleb-1023 <okaleb106@gmail.com>
2023-05-20 16:18:47 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import CountdownTimer from "./compononets/countdown_timer/countdown_timer.component"
+
+function App() {
+
+  return (
+    <>
+      <CountdownTimer />
+    </>
+  )
+}
+
+export default App
---
+++ src/compononets/countdown_timer/countdown_timer.component.jsx
@@ -0,0 +1,43 @@
+import Countdown, { zeroPad } from "react-countdown"
+import './countdown_timer.style.scss'
+
+const renderer = ({ days, hours, minutes, seconds }) => {
+    
+    // Render a countdown
+    return (
+        <div className="counter-container">
+            <div className="counter">
+                <div className="count-container">
+                    <h1 className="count">{zeroPad(days)}</h1>
+                    <p className="description">days</p>
+                </div>
+                <span className="colon">:</span>
+                <div className="count-container">
+                    <h1 className="count">{zeroPad(hours)}</h1>
+                    <p className="description">hours</p>
+                </div>
+                <span className="colon">:</span>
+                <div className="count-container">
+                    <h1 className="count">{zeroPad(minutes)}</h1>
+                    <p className="description">minutes</p>
+                </div>
+                <span className="colon">:</span>
+                <div className="count-container">
+                    <h1 className="count">{zeroPad(seconds)}</h1>
+                    <p className="description">seconds</p>
+                </div>
+            </div>
+        </div>
+    );
+  };
+
+const CountdownTimer = () => {
+  return (
+    <div className="countdown_timer">
+        <h1>Coming Soon</h1>
+        <Countdown date={'2023-06-29T09:00:00'} renderer={renderer} />
+    </div>
+  )
+}
+
+export default CountdownTimer
